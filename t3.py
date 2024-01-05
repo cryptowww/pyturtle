05cd99cef43aeb5a17275512895b24e2889964a9
James Kotling <3201920+cryptowww@users.noreply.github.com>
2024-01-05 11:39:03 +0800

    add settings

--- t3.py
+++ t3.py
@@ -4,8 +4,20 @@ turtle 画虚线
 """
 import turtle as t
 
+# 设置画笔速度
+t.speed(0)
+
+# 画虚线
 for step in range(100):
-    t.fd(5)
+    t.fd(3)
     t.up() if t.isdown()  else t.down()
+
+# 移动画笔，并画点
+t.up()
+t.goto(-200, 200)
+t.down()
+t.dot(10, 'red')
+# 隐藏画笔
+t.hideturtle()
 
 t.mainloop()
